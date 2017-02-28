26aa645183b4dbd966d36bf863310fed1b67a90e
Кисель Ян <kisel@corp.mail.ru>
2017-02-28 17:57:47 +0300

    initial import

---
+++ src/main/java/WordCountJob.java
@@ -0,0 +1,69 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+public class WordCountJob extends Configured implements Tool {
+    public static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            // split by space symbols (space, tab, ...)
+            for(String word: line.split("\\s"))
+                context.write(new Text(word), new IntWritable(1));
+        }
+    }
+
+    public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int summ = 0;
+            for(IntWritable i: values) {
+                summ += i.get();
+            }
+
+            // produce pairs of "word" <-> amount
+            context.write(key, new IntWritable(summ));
+        }
+    }
+
+    private Job getJobConf(String input, String output) throws IOException {
+        Job job = Job.getInstance(getConf());
+        job.setJarByClass(WordCountJob.class);
+        job.setJobName(WordCountJob.class.getCanonicalName());
+
+        // will use traditional TextInputFormat to split line-by-line
+        TextInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        return job;
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = getJobConf(args[0], args[1]);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    static public void main(String[] args) throws Exception {
+        int ret = ToolRunner.run(new WordCountJob(), args);
+        System.exit(ret);
+    }
+}
